91956fb7ee6fb7c6f3354ccc611e0a072d8eb54e
Verra-ade <stephanie.adeniyi5@gmail.com>
2022-03-13 05:19:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Print message
  *
- * Return: Always 0 (Sucess)
+ *  Return: Always 0 (Success)
  *
  */
 int main(void)
@@ -11,4 +11,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
