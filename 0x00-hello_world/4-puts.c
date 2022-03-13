36f6817803e6bc0510ea433ddcd4e82371d21777
Verra-ade <stephanie.adeniyi5@gmail.com>
2022-03-13 05:01:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 /**
- * main -Print message
+ * main - Print message
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  *
  */
-int main (void)
-{puts("\"Programming is like building a multilingual puzzle"); return (0);}
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
+
