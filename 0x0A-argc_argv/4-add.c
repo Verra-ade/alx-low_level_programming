9440a9b9abca57b413cf2d8f6e7df8f455b178a3
Verra-ade <stephanie.adeniyi5@gmail.com>
2022-04-06 18:51:26 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,73 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return; Always 0 (Success)
+ * _isdigit - Check for a digit (0 through 9).
+ * @c: Charater to check
+ * Return: 1 if c is a digit, return 0 otherwise.
  */
 
-int check_num(char *str)
-
+int _isdigit(int c)
 {
-/*Declaring variables*/
-unsigned int count;
-
-count = 0;
-while (count < strlen(str)) /*count string*/
-
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
-
-count++;
-}
-return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * return: Always 0 (Success)
+ * main - Add the given numbers.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 is succes.
  */
-
 int main(int argc, char *argv[])
-
 {
+	int i, j, radd = 0;
+	char *a = NULL;
 
-/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
+	if (argc <= 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if(check_num(argv[count]))
+	for (i = 1; i < argc; i++)
+	{
+		a = argv[i];
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (_isdigit(a[j]) != 1)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		radd += atoi(argv[i]);
+	}
 
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-sum += str_to_int;
-}
+	printf("%d\n", radd);
 
-/*Condition if one of the number contains symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
-
-count++;
-}
-
-printf("%d\n", sum); /*print sum*/
-
-return (0);
+	return (0);
 }
